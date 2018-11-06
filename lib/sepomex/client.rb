526c4d3cc142eec70e42b2f7f57ad6d05560f289
Nora Gabriela Alvarado <noragmora@gmail.com>
2018-11-06 10:16:46 -0600

    Initial commit

---
+++ lib/sepomex/client.rb
@@ -0,0 +1,7 @@
+require "sepomex/client/version"
+
+module Sepomex
+  module Client
+    # Your code goes here...
+  end
+end
---
+++ lib/sepomex/client/version.rb
@@ -0,0 +1,5 @@
+module Sepomex
+  module Client
+    VERSION = "0.1.0"
+  end
+end
