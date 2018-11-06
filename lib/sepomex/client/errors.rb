493a4076416e06b939c0b105806fc88a92100d2d
Nora Gabriela Alvarado <noragmora@gmail.com>
2018-11-06 11:41:06 -0600

    Remove unused errors

--- lib/sepomex/client/errors.rb
+++ lib/sepomex/client/errors.rb
@@ -3,7 +3,5 @@ module Sepomex
     SepomexClientError = Class.new StandardError
     BadRequestError = Class.new StandardError
     ServerError = Class.new StandardError
-    PrivateKeyError = Class.new StandardError
-    NotPrivateKeyError = Class.new ArgumentError
   end
 end
