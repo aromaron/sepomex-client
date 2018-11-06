6be21d623320b44143c7ac29550c322a9b304fb6
Nora Gabriela Alvarado <noragmora@gmail.com>
2018-11-06 10:57:30 -0600

    Add better error handling

--- lib/sepomex/client/base.rb
+++ lib/sepomex/client/base.rb
@@ -1,6 +1,17 @@
 module Sepomex
   module Client
     class Base < Flexirest::Base
+      after_request :rescue_errors
+
+      def rescue_errors(response)
+        if response.status == 400 # bad request
+          result = JSON.parse(response.body)
+          message = result['errors'] || [result['error']]
+          raise BadRequestError, message.to_a.join(', ')
+        elsif (500..599).cover?(response.status)
+          raise ServerError, 'Server could not process request'
+        end
+      end
     end
   end
 end
--- lib/sepomex/client/errors.rb
+++ lib/sepomex/client/errors.rb
@@ -1,5 +1,9 @@
 module Sepomex
   module Client
     SepomexClientError = Class.new StandardError
+    BadRequestError = Class.new StandardError
+    ServerError = Class.new StandardError
+    PrivateKeyError = Class.new StandardError
+    NotPrivateKeyError = Class.new ArgumentError
   end
 end
